552cd2679b6048887bf50b9ec8d357fe37056518
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-10-23 11:26:49 -0700

    feat: range function in utils.js

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+export function range(length) {
+  if(length <= 0) throw new Error("Please enter a number greater than cero");
+  
+  let arr = []
+  
+  for (let index = 0; index < length; index++) {
+    arr.push(index);
+  }
+  
+  return arr;
+}
