4b03517b89155f646c55e5b6a4389070498a04b1
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-10-25 11:26:15 -0700

    fix: styles for color inputs form

--- src/components/NumberOfColorsForm/NumberOfColorsForm.jsx
+++ src/components/NumberOfColorsForm/NumberOfColorsForm.jsx
@@ -1,9 +1,12 @@
 import { useContext, useState } from "react";
 import { Check, Plus } from "react-feather";
 
+import { ColorsContext } from "../../providers/ColorsProvider.jsx";
+
 import Button from "../Button/Button.jsx";
 import ColorInput from "../ColorInput/ColorInput.jsx";
-import { ColorsContext } from "../../providers/ColorsProvider.jsx";
+
+import styles from './NumberOfColorsForm.module.css'
 
 function NumberOfColorsForm() {
   const [numberOfColors, setNumberOfColors] = useState(0);
@@ -23,30 +26,36 @@ function NumberOfColorsForm() {
   }
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <label htmlFor="number-of-colors">Numero de colores en tu paleta:</label>
-      <input
-        type="number"
-        id="number-of-colors"
-        value={numberOfColors}
-        onChange={(e) => {
-          setAreColorInputsShowing(false);
-          setNumberOfColors(e.target.value);
-        }}
-      />
-      <Button clickFunction={handleConfirmButton} icon={<Check />}>
-        confirmar
-      </Button>
+    <form className={styles.numberOfColorsForm} onSubmit={(e) => e.preventDefault()}>
+      <div className={styles.numberOfColorsContainer}>
+        <label htmlFor="number-of-colors">Numero de colores en tu paleta:</label>
+        <div className={styles.numberOfColorsInputs}>
+          <input
+            type="number"
+            id="number-of-colors"
+            value={numberOfColors}
+            onChange={(e) => {
+              setAreColorInputsShowing(false);
+              setNumberOfColors(e.target.value);
+            }}
+          />
+          <Button clickFunction={handleConfirmButton} icon={<Check />}>
+            confirmar
+          </Button>
+        </div>
+      </div>
       {areColorInputsShowing && (
-        <div>
+        <div className={styles.colorInputsContainer}>
           {colors.map(({id, value}, index) => (
             <ColorInput key={id} id={id} value={value} colorNumber={index + 1} />
           ))}
         </div>
       )}
-      <Button clickFunction={handleNewColor} icon={<Plus />}>
-        agregar un color
-      </Button>
+      <div className={styles.addColorButton}>
+        <Button clickFunction={handleNewColor} icon={<Plus />}>
+          agregar un color
+        </Button>
+      </div>
     </form>
   );
 }
