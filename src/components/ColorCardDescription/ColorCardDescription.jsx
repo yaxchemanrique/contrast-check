a360c9aeb6dab3d3b2621519cac1cdf5d31b70a1
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-10-25 11:30:34 -0700

    feat: copy text to clipboard

--- src/components/ColorCardDescription/ColorCardDescription.jsx
+++ src/components/ColorCardDescription/ColorCardDescription.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import {Copy} from 'react-feather'
+import React from "react";
+import { Copy } from "react-feather";
 
-import styles from './ColorCardDescription.module.css'
+import styles from "./ColorCardDescription.module.css";
 
-function ColorCardDescription({background, foreground}) {
+function ColorCardDescription({ background, foreground }) {
+  const clipboardText = `background-color: ${background}; color: ${foreground};`;
+
+  async function handleCopyClick() {
+    try {
+      await window.navigator.clipboard.writeText(clipboardText);
+      alert("Copied to clipboard!");
+    } catch (err) {
+      console.error("Unable to copy to clipboard.", err);
+      alert("Copy to clipboard failed.");
+    }
+  }
+
   return (
     <div className={styles.colorCardDescription}>
       <div className={styles.iconContainer}>
-        <button>
-          <Copy size={18}/>
+        <button onClick={handleCopyClick}>
+          <Copy size={18} />
         </button>
       </div>
-      <p>fondo: <span>{background}</span></p>
-      <p>texto: <span>{foreground}</span></p>
+      <p>
+        fondo: <span>{background}</span>
+      </p>
+      <p>
+        texto: <span>{foreground}</span>
+      </p>
     </div>
-  )
+  );
 }
 
-export default ColorCardDescription
+export default ColorCardDescription;
