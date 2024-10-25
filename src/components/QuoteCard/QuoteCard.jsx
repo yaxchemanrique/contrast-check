7b1e02fb436193a0924571068d9ee314cdfe59ad
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-10-25 11:28:25 -0700

    feat: separation of components for cards with selection and without selection

--- src/components/QuoteCard/QuoteCard.jsx
+++ src/components/QuoteCard/QuoteCard.jsx
@@ -1,22 +1,64 @@
-import { useState } from "react";
+import { useContext, useId, useState } from "react";
 import { CheckSquare, Square } from "react-feather";
+
+import { ColorsContext } from "../../providers/ColorsProvider";
 
 import styles from "./QuoteCard.module.css";
 
+export function QuoteCardWithoutSelection({ background, foreground }) {
+  return (
+    <article
+      style={{ backgroundColor: background, color: foreground }}
+      className={`${styles.quoteCard} ${styles.quoteCardWithoutSelection}`}
+    >
+      <p className={styles.title}>Quote no.28</p>
+      <p className={styles.content}>
+        I like design, I like details, to me it is just another form of
+        self-expression.
+      </p>
+      <address rel="author">John Malkovich</address>
+    </article>
+  );
+}
+
 function QuoteCard({ id, background, foreground }) {
   const [isChecked, setIsChecked] = useState(false);
+  const { handleComboSelection } = useContext(ColorsContext);
+
+  const idHook = useId();
+  const checkboxId = `${idHook}-${id}`;
 
   return (
     <article
       style={{ backgroundColor: background, color: foreground }}
       className={styles.quoteCard}
     >
-      <label htmlFor={id}>
+      <label htmlFor={checkboxId}>
         <span className={`${styles.checkbox} `}>
-          <CheckSquare className={`${isChecked ? styles.show : styles.hidden}`} size={20}/>
-          <Square className={`${isChecked ? styles.hidden : styles.show}`} size={20}/>
+          <CheckSquare
+            className={`${isChecked ? styles.show : styles.hidden}`}
+            size={20}
+          />
+          <Square
+            className={`${isChecked ? styles.hidden : styles.show}`}
+            size={20}
+          />
         </span>
-        <input type="checkbox" id={id} value={isChecked} onChange={(e) => setIsChecked(e.target.checked)}/>
+        <input
+          type="checkbox"
+          id={checkboxId}
+          value={isChecked}
+          onChange={(e) => {
+            setIsChecked(e.target.checked);
+            if (e.target.checked) {
+              console.log("checked", id);
+              handleComboSelection("add", id);
+            } else {
+              console.log("UNchecked", id);
+              handleComboSelection("remove", id);
+            }
+          }}
+        />
         <p className={styles.title}>Quote no.28</p>
         <p className={styles.content}>
           I like design, I like details, to me it is just another form of
